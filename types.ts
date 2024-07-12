81cd9e3842f7680e04c4f2e12d667db033ee5c53
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2024-07-12 00:10:32 -0300

    feat: update types

--- types.ts
+++ types.ts
@@ -1,10 +1,17 @@
+export interface Media {
+  id: string
+  label: string
+  url: string
+  participantId: string
+}
+
 export interface Participant {
-  id: string;
-  name: string;
+  id: string
+  name: string
 }
 
 export interface Routes {
-  href: string,
-  label: string,
+  href: string
+  label: string
   active: boolean
 }
