1e2691c047ae900705307f82b9d6feeacbf700f4
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2024-07-22 15:55:04 -0300

    feat: create chunk array into utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,15 @@
+import { Media } from "@/types";
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export const chunkMedias = (array: Media[], size: number) => {
+  const chunkedMedias = [];
+  for (let i = 0; i < array?.length; i += size) {
+    chunkedMedias.push(array.slice(i, i + size));
+  }
+  return chunkedMedias;
 }
