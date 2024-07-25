3aedf522ed4e78e38b7a55476980e2fffbdf8822
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2024-07-24 23:54:14 -0300

    feat: add tag in media modal

--- components/modals/media-modal.tsx
+++ components/modals/media-modal.tsx
@@ -2,14 +2,21 @@
 
 import useMediaModal from "@/hooks/use-media-modal";
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "../ui/dialog";
+import Link from "next/link";
+import useParticipantStore from "@/hooks/use-participant-store";
 
 const MediaModal = () => {
   const mediaModal = useMediaModal();
   const media = useMediaModal((state) => state.data);
+  const { participants } = useParticipantStore();
 
   if (!media) { 
     return null;
   }
+
+  const participant = participants.find((participant) => participant.id === media.participantId);
+
+  const { id, numericId, label, url  } = media;
 
   const onChange = (open: boolean) => {
     if (!open) {
@@ -21,13 +28,20 @@ const MediaModal = () => {
     <Dialog open={mediaModal.isOpen} onOpenChange={onChange}>
       <DialogContent className="max-w-[90vw] sm:max-w-[60vw] border-0">
         <DialogHeader>
-          <DialogTitle>#{media.numericId} - {media.label}</DialogTitle>
+          <DialogTitle className="flex gap-2 items-center">
+            {participant && (
+              <Link href={`/medias?participantId=${participant.id}`} onClick={() => onChange(false)} className="flex px-2 py-1 rounded-md" style={{ backgroundColor: participant.bgColor }}>
+                <span className="font-semibold text-md uppercase" style={{ color: participant.txtColor }}>{participant.name}</span>
+              </Link>
+            )}
+            #{numericId} - {label}
+          </DialogTitle>
           <DialogDescription>
-            {media.id}
+            {id}
           </DialogDescription>
         </DialogHeader>
         <div className="w-full">
-          <video src={media.url} autoPlay controls className="w-full rounded-3xl"></video>
+          <video src={url} autoPlay controls className="w-full rounded-3xl"></video>
         </div>
       </DialogContent>
     </Dialog>
